d35e7a9b8307a25329f6f90308781ded835e8519
zeh <zeh@zehfernando.com>
2015-02-18 15:54:29 -0500

    Rebuilt

---
+++ js/signals/SimpleSignal.js
@@ -0,0 +1,54 @@
+var zehfernando;
+(function (zehfernando) {
+    var signals;
+    (function (signals) {
+        var SimpleSignal = (function () {
+            function SimpleSignal() {
+            }
+            SimpleSignal.prototype.SimpleSignal = function () {
+                this.functions = [];
+            };
+            SimpleSignal.prototype.add = function (func) {
+                if (this.functions.indexOf(func) == -1) {
+                    this.functions.push(func);
+                    return true;
+                }
+                return false;
+            };
+            SimpleSignal.prototype.remove = function (func) {
+                this.ifr = this.functions.indexOf(func);
+                if (this.ifr > -1) {
+                    this.functions.splice(this.ifr, 1);
+                    return true;
+                }
+                return false;
+            };
+            SimpleSignal.prototype.removeAll = function () {
+                if (this.functions.length > 0) {
+                    this.functions.length = 0;
+                    return true;
+                }
+                return false;
+            };
+            SimpleSignal.prototype.dispatch = function () {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i - 0] = arguments[_i];
+                }
+                var functionsDuplicate = this.functions.concat();
+                for (var i = 0; i < functionsDuplicate.length; i++) {
+                    functionsDuplicate[i].apply(undefined, args);
+                }
+            };
+            Object.defineProperty(SimpleSignal.prototype, "numItems", {
+                get: function () {
+                    return this.functions.length;
+                },
+                enumerable: true,
+                configurable: true
+            });
+            return SimpleSignal;
+        })();
+    })(signals = zehfernando.signals || (zehfernando.signals = {}));
+})(zehfernando || (zehfernando = {}));
+//# sourceMappingURL=SimpleSignal.js.map
---
+++ js/utils/MathUtils.js
@@ -0,0 +1,59 @@
+var zehfernando;
+(function (zehfernando) {
+    var utils;
+    (function (utils) {
+        var MathUtils = (function () {
+            function MathUtils() {
+            }
+            MathUtils.clamp = function (value, min, max) {
+                if (min === void 0) { min = 0; }
+                if (max === void 0) { max = 1; }
+                return value < min ? min : value > max ? max : value;
+            };
+            MathUtils.clampAuto = function (value, clamp1, clamp2) {
+                if (clamp1 === void 0) { clamp1 = 0; }
+                if (clamp2 === void 0) { clamp2 = 1; }
+                if (clamp2 < clamp1) {
+                    var v = clamp2;
+                    clamp2 = clamp1;
+                    clamp1 = v;
+                }
+                return value < clamp1 ? clamp1 : value > clamp2 ? clamp2 : value;
+            };
+            MathUtils.map = function (value, oldMin, oldMax, newMin, newMax, clamp) {
+                if (newMin === void 0) { newMin = 0; }
+                if (newMax === void 0) { newMax = 1; }
+                if (clamp === void 0) { clamp = false; }
+                if (oldMin == oldMax)
+                    return newMin;
+                this.map_p = ((value - oldMin) / (oldMax - oldMin) * (newMax - newMin)) + newMin;
+                if (clamp)
+                    this.map_p = newMin < newMax ? this.clamp(this.map_p, newMin, newMax) : this.clamp(this.map_p, newMax, newMin);
+                return this.map_p;
+            };
+            MathUtils.rangeMod = function (value, min, pseudoMax) {
+                var range = pseudoMax - min;
+                value = (value - min) % range;
+                if (value < 0)
+                    value = range - (-value % range);
+                value += min;
+                return value;
+            };
+            MathUtils.isPowerOfTwo = function (value) {
+                while (value % 2 == 0 && value > 2)
+                    value /= 2;
+                return value == 2;
+            };
+            MathUtils.getHighestPowerOfTwo = function (value) {
+                var c = 1;
+                while (c < value)
+                    c *= 2;
+                return c;
+            };
+            MathUtils.DEG2RAD = 1 / 180 * Math.PI;
+            MathUtils.RAD2DEG = 1 / Math.PI * 180;
+            return MathUtils;
+        })();
+    })(utils = zehfernando.utils || (zehfernando.utils = {}));
+})(zehfernando || (zehfernando = {}));
+//# sourceMappingURL=MathUtils.js.map
